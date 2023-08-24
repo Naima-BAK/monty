05a2a3188a62233f215efd279198f4edd6a319fb
Naima-BAK <n.bakenchich@gmail.com>
2023-08-24 13:51:31 -0700

    test

--- monty.h
+++ monty.h
@@ -1,17 +1,14 @@
-#ifndef MONTY_H
-#define MONTY_H
-#include <string.h>
+#ifndef __MONTY__H
+#define __MONTY__H
+
 #include <stdio.h>
-#include <stdlib.h>
 #include <unistd.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
+#include <string.h>
+#include <stdlib.h>
 #include <ctype.h>
-#define UNUSED(x) (void)(x)
-#define _POSIX_C_SOURCE 200809L
-extern char *global_data;
-/* Data structures */
+#include <stdarg.h>
+
+
 
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
@@ -20,51 +17,71 @@ extern char *global_data;
  * @next: points to the next element of the stack (or queue)
  *
  * Description: doubly linked list node structure
- * for stack, queues, LIFO, FIFO
+ * for stack, queues, LIFO, FIFO Holberton project
  */
 typedef struct stack_s
 {
-int n;
-struct stack_s *prev;
-struct stack_s *next;
+	int n;
+	struct stack_s *prev;
+	struct stack_s *next;
 } stack_t;
 
 /**
- * struct instruction_s - opcode and its function
+ * struct instruction_s - opcoode and its function
  * @opcode: the opcode
  * @f: function to handle the opcode
  *
  * Description: opcode and its function
- * for stack, queues, LIFO, FIFO
+ * for stack, queues, LIFO, FIFO Holberton project
  */
 typedef struct instruction_s
 {
-char *opcode;
-void (*f)(stack_t **stack, unsigned int line_number);
+	char *opcode;
+	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
-/*opcode*/
-void f_pall(stack_t **stack, unsigned int line_num);
-void f_push(stack_t **stack, unsigned int line_num);
-void f_pint(stack_t **stack, unsigned int line_num);
-void f_pop(stack_t **stack, unsigned int line_num);
-void f_swap(stack_t **stack, unsigned int line_num);
-void f_add(stack_t **stack, unsigned int line_num);
-void f_nop(stack_t **stack, unsigned int line_num);
-void f_sub(stack_t **stack, unsigned int line_num);
-void f_div(stack_t **stack, unsigned int line_num);
-void f_mul(stack_t **stack, unsigned int line_num);
-void f_mod(stack_t **stack, unsigned int line_num);
-void f_pchar(stack_t **stack, unsigned int line_num);
-void f_pstr(stack_t **stack, unsigned int line_num);
-void f_rotl(stack_t **stack, unsigned int line_num);
-void f_rotr(stack_t **stack, unsigned int line_num);
-void f_queue(stack_t **stack, unsigned int line_num);
-void f_stack(stack_t **stack, unsigned int line_num);
 
-/* other functions */
-void (*get_opc(char *opc))(stack_t **stack, unsigned int line_num);
-int main(int argc, char *argv[]);
-void process_l(stack_t **stack, char *line, unsigned int line_num);
-void read_monty_f(const char *file);
-ssize_t getline(char **lineptr, size_t *n, FILE *stream);
-#endif
+extern stack_t *head;
+
+/*Type for opcode functions*/
+typedef void (*op_func)(stack_t **, unsigned int);
+
+/*File operations*/
+void open_file(char *);
+void read_file(FILE *);
+int len_chars(FILE *);
+int interpret_line(char *, int, int);
+void find_func(char *, char *, int, int);
+
+/*Stack operations*/
+stack_t *create_node(int n);
+void free_nodes(void);
+void print_stack(stack_t **, unsigned int);
+void add_to_stack(stack_t **, unsigned int);
+void add_to_queue(stack_t **, unsigned int);
+
+void call_fun(op_func, char *, char *, int, int);
+void print_top(stack_t **, unsigned int);
+void pop_top(stack_t **, unsigned int);
+void nop(stack_t **, unsigned int);
+void swap_nodes(stack_t **, unsigned int);
+
+/*Math operations with nodes*/
+void add_nodes(stack_t **, unsigned int);
+void sub_nodes(stack_t **, unsigned int);
+void div_nodes(stack_t **, unsigned int);
+void mul_nodes(stack_t **, unsigned int);
+void mod_nodes(stack_t **, unsigned int);
+
+/*String operations*/
+void print_char(stack_t **, unsigned int);
+void print_str(stack_t **, unsigned int);
+void rotl(stack_t **, unsigned int);
+
+/*Error hanlding*/
+void err(int error_code, ...);
+void more_err(int error_code, ...);
+void string_err(int error_code, ...);
+void rotr(stack_t **, unsigned int);
+
+
+#endif /*__MONTY__H*/
