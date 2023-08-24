046205684a9e26f5e6ae65475172c158717a2810
Naima-BAK <n.bakenchich@gmail.com>
2023-08-24 13:04:17 -0700

    test

--- monty.c
+++ monty.c
@@ -1,11 +1,10 @@
 #include "monty.h"
 /**
- * main - main function
- * @argc: number of arguments.
- * @argv: pointer to array of arguumenst.
- * Return: failure, or success
+ * main - entry point to program
+ * @argc: number of arguments
+ * @argv: pointer to array of args
+ * Return: failure or success
  */
-
 int main(int argc, char *argv[])
 {
 if (argc != 2)
@@ -13,6 +12,6 @@ if (argc != 2)
 fprintf(stderr, "USAGE: monty file\n");
 return (EXIT_FAILURE);
 }
-monty_bytes(argv[1]);
+read_monty_f(argv[1]);
 return (EXIT_SUCCESS);
 }
--- monty.h
+++ monty.h
@@ -1,19 +1,17 @@
 #ifndef MONTY_H
 #define MONTY_H
-
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/stat.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include <fcntl.h>
 #include <ctype.h>
-
-extern char *vallData;
-
 #define UNUSED(x) (void)(x)
 #define _POSIX_C_SOURCE 200809L
+extern char *global_data;
+/* Data structures */
 
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
@@ -31,9 +29,6 @@ struct stack_s *prev;
 struct stack_s *next;
 } stack_t;
 
-
-
-
 /**
  * struct instruction_s - opcode and its function
  * @opcode: the opcode
@@ -47,19 +42,29 @@ typedef struct instruction_s
 char *opcode;
 void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
+/*opcode*/
+void f_pall(stack_t **stack, unsigned int line_num);
+void f_push(stack_t **stack, unsigned int line_num);
+void f_pint(stack_t **stack, unsigned int line_num);
+void f_pop(stack_t **stack, unsigned int line_num);
+void f_swap(stack_t **stack, unsigned int line_num);
+void f_add(stack_t **stack, unsigned int line_num);
+void f_nop(stack_t **stack, unsigned int line_num);
+void f_sub(stack_t **stack, unsigned int line_num);
+void f_div(stack_t **stack, unsigned int line_num);
+void f_mul(stack_t **stack, unsigned int line_num);
+void f_mod(stack_t **stack, unsigned int line_num);
+void f_pchar(stack_t **stack, unsigned int line_num);
+void f_pstr(stack_t **stack, unsigned int line_num);
+void f_rotl(stack_t **stack, unsigned int line_num);
+void f_rotr(stack_t **stack, unsigned int line_num);
+void f_queue(stack_t **stack, unsigned int line_num);
+void f_stack(stack_t **stack, unsigned int line_num);
 
-
-void pall_function(stack_t **stack_head, unsigned int index);
-void push_function(stack_t **stack_head, unsigned int index);
-ssize_t getline(char **linePointer, size_t *n, FILE *fileS);
-void monty_bytes(const char *theFile);
-void process_function(stack_t **stack, char *ligne, unsigned int counter);
-void (*allOperations(char *opcode_search))(stack_t **stack, unsigned int line_num);
-void pint_function(stack_t **stack, unsigned int line_num);
-void pop_function(stack_t **stack, unsigned int line_num);
-void swap_function(stack_t **stack, unsigned int line_num);
-void add_function(stack_t **stack, unsigned int line_num);
-void nop_function(stack_t **stack, unsigned int line_num);
-
-
+/* other functions */
+void (*get_opc(char *opc))(stack_t **stack, unsigned int line_num);
+int main(int argc, char *argv[]);
+void process_l(stack_t **stack, char *line, unsigned int line_num);
+void read_monty_f(const char *file);
+ssize_t getline(char **lineptr, size_t *n, FILE *stream);
 #endif
